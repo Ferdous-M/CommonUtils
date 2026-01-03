035785c1dbbfa2f83ff88817d617a341b34326d3
Ferdous-M <mahzabinferdous000@gmail.com>
2026-01-03 12:28:49 +0600

    added shared DTO for Kafka

---
+++ src/main/java/com/immo2n/citypass/DTOS/RideEventDTO.java
@@ -0,0 +1,13 @@
+package com.immo2n.citypass.DTOS;
+import lombok.Data;
+
+@Data
+public class RideEventDTO {
+    private String rideId;
+    private double pickupLat;
+    private double pickupLng;
+    private Double destinationLat;
+    private Double destinationLng;
+    private String preferences;
+    private long timestamp;
+}
